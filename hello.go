688774647eee1dd02c8756c2cf4c0a82b6be8be3
Gaurav <gauravrathore2u@gmail.com>
2024-12-06 14:36:53 +0530

    hello world

---
+++ hello.go
@@ -0,0 +1,15 @@
+//go is a compiled language
+//go is a statically typed language
+//go is a garbage collected language
+//go is a concurrent language
+//go compiles faster than java
+
+//to run go file: go run hello.go
+
+package main
+
+import "fmt";
+
+func main() {
+	fmt.Println("Hello World!");
+}
