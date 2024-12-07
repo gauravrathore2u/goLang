1c63b809d9972537a2090613e99cf38045f3f079
Gaurav <gauravrathore2u@gmail.com>
2024-12-07 13:20:06 +0530

    struct collection

--- hello.go
+++ hello.go
@@ -175,8 +175,61 @@ func main() {
 
 
 
+	//Struct
+	//---------------------------------------------------------------------------------------------
+	//struct is similar to the object in javascript and dictionary in python
+	type Address struct {
+		City string;  // all values are assigned initial values as "" or 0 or false
+		State string;
+	}
+	type Person struct {
+		Name string;
+		Age int;
+		IsStudent bool;
+		PersonAddress Address;
+	}
+	
+	p2  := Person{}; // initialize the struct
+	var p1 Person = Person{"John", 30, true, Address{"New York", "NY"}};
+	fmt.Println(p1);
+	fmt.Println((p2.IsStudent));
+
+	//Embedded struct
+	type Person1 struct {
+		Address;  // embedded struct, now all the properties of Address will be available
+		Name string;
+		Age int;
+	}
+	type Address1 struct {
+		City string;
+		State string;
+	}
+	p3 := Person1{};
+	p4 := Person1{Address{"New York", "NY"}, "John", 30};
+	fmt.Println(p3);
+	fmt.Println(p4.City); // now we can directly access the property of Address
+
+
+	//Struct methods
+	//---------------------------------------------------------------------------------------------
+	p5 := rect{10, 20};
+	fmt.Println(p5.area());
 	
 }
+
+type rect struct {
+	width  int
+	height int
+	// newArea func() //we can also give methods to struct but this will loose the 
+					   //singularity we separatly has to save the area in function , 
+					// if someone change the width in struct and forgot to change the area then it will be wrong
+					// we can also give methods to struct by declaring struct before fn name
+}
+// we can give methods to struct by declaring struct before fn name
+func (r rect) area() int {
+	return r.width * r.height
+}
+
 
 func sub(a int, b int) int {
 	return a - b;
