81d5e56875eb97cb9ad6419ecab61863fdf5b374
Gaurav <gauravrathore2u@gmail.com>
2024-12-11 13:50:53 +0530

    pointers

--- pointers/index.go
+++ pointers/index.go
@@ -7,5 +7,10 @@ func main() {
 	var b *int = &a;  //b is a pointer to address of a
 	fmt.Println(a); //10
 	fmt.Println(b); //address of a
-	fmt.Println(*b); //value at address of a i.e. 10
+	fmt.Println(*b); //dereference //value at address of a i.e. 10
+
+	//Imp:- Pointers can be dangerous
+	//If a pointer points to nothing (zero value of the pointer type ) then dereferencing it will cause a run time error.
+	//that crashes the program.
+	//whenever dealing with pointers always check if the pointer is nil before dereferencing it.
 }
